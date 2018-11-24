fddabe634abe4b6bfb3df641e27167184bf1621b
Bahaa Gabal <belgabal@trudoc24x7.com>
2018-11-24 17:11:38 +0200

    Organize Gradle

---
+++ app/src/main/java/dp/schoolandroid/di/modules/NetworkModule.java
@@ -0,0 +1,44 @@
+package dp.schoolandroid.di.modules;
+
+import android.content.Context;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dp.schoolandroid.R;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Bahaa Gabal on 24,November,2018
+ */
+
+@Module
+public class NetworkModule {
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(15, TimeUnit.SECONDS).build();
+        return okHttpClient;
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Context context, OkHttpClient okHttpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(context.getString(R.string.base_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build();
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/dp/schoolandroid/service/model/Class1.java
@@ -0,0 +1,7 @@
+package dp.schoolandroid.service.model;
+
+/**
+ * Created by Bahaa Gabal on 24,November,2018
+ */
+public class Class1 {
+}
